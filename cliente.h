466d4168d15aaa853edc2493724eadafa1e5b25f
wagnerkenhiti <w31j.nakamura@gmail.com>
2024-02-29 16:08:58 -0400

    salvando alteracoes da bianca

--- cliente.h
+++ cliente.h
@@ -15,5 +15,43 @@ typedef struct CLIENTE
     unsigned int pontos;
 } CLIENTE;
 
+/**
+ * Função que adiciona um novo cliente através do teclado
+ * @param c Ponteiro para um registro do tipo CLIENTE
+*/
+void adicionarCliente(CLIENTE *c);
+
+/**
+ * Grava dados de um cliente em texto, caso o arquivo não exista, cria ele
+ * @param c Cliente salvo no registro
+*/
+int gravarClienteCSV(CLIENTE c);
+
+/**
+ * Grava dados de um cliente em binário, caso o arquivo
+ * não exista, cria ele
+ * @param c Cliente salvo no registro 
+*/
+int gravarClienteDAT(CLIENTE c);
+
+/**
+ * Edita dados no CSV do cliente a partir de uma busca com o CPF
+*/
+int editaClienteCSV();
+
+/**
+ * Edita dados no DAT do cliente a partir de uma busca com o CPF
+*/
+int editaClienteDAT();
+
+/**
+ * Lista clientes entre 18 a 25 anos
+*/
+int listaClientes();
+
+/**
+ * Procura cliente a partir do CPF
+*/
+bool procuraCliente();
 
 #endif
